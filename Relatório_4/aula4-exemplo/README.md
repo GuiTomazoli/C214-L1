aba18a9b09cea861ab7bdda245b9b5c1257e841c
Guilherme Tomazoli de Carvalho <guilherme.tomazoli@gec.inatel.br>
2023-09-06 14:54:26 -0300

    Adicionar Relatório_4

---
+++ Relatório_4/aula4-exemplo/README.md
@@ -0,0 +1,11 @@
+# Aula 04
+
+Foi feito um botão de marcar GOL, quando o usuário interagir com ele ao apertar o botão imprimirá a mensagem no console e exibirá uma notificação de gol na interface do usuário.
+
+Implementações: 
+
+- **Biblioteca sweetalert2**: Foi implementando uma biblioteca no código chamada de "sweetalert2", onde mostrará uma notificação ao usuário quando ele apertar o botão.
+- **Centralização**: Foram centralizados a mensagem de "Exibe a informação quando marca GOL" e o botão de "Marcar GOL", para ficar melhor a visualização para o usuário.
+
+
+
---
+++ Relatório_4/aula4-exemplo/src/Jogo.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Jogador from "./Jogador.svelte";
+	import Swal from 'sweetalert2';
+	
+	function marcaGol(){
+		console.log("Marcou 1 GOL");
+		Swal.fire({
+			title: 'GOL!',
+			text: 'Você marcou um gol!',
+			icon: 'success',
+			showConfirmButton: false,
+			timer: 1500 // Tempo em milissegundos para a notificação desaparecer
+		});
+	}
+</script>
+
+<main>
+	<h1> Exibe a informação quando marca GOL </h1>
+	<Jogador acao = {marcaGol} />	
+</main>
+
+<style>
+	main {
+	  font-family: sans-serif;
+	  text-align: center;
+	  padding: 2rem;
+	}
+  
+	h1 {
+	  font-size: 1.5rem;
+	  margin-bottom: 1rem;
+	}
+  
+  </style>
+
+ 
